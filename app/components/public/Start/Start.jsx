16f6dac27edcd832c473990938ed7819f13cc034
Boning Li <344270545@qq.com>
2019-04-03 23:57:08 +0200

    add ranking,map to homepage;fix some bug;refactor ForTheThrone and start.jsx

--- app/components/public/Start/Start.jsx
+++ app/components/public/Start/Start.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 let { Component } = React;
 import { Row, Col, Grid } from 'react-bootstrap';
 
-import ForTheThrone from '../../common/ForTheThrone/ForTheThrone.jsx';
 import './Start.css';
 
-import CharacterList from 'json!../../common/ForTheThrone/antagonistCharacters.json';
-
-
+import antagonistCharacters from 'json!./antagonistCharacters.json';
+import ForTheThrone from '../../common/ForTheThrone/ForTheThrone.jsx';
 import PlodTop5 from '../../common/Ranking/PlodTop5';
 import Survivors from '../../common/Ranking/Survivors';
 import MapComp from '../../common/MapComp/MapComp.jsx';
@@ -16,16 +14,33 @@ export default class Start extends Component {
 
   constructor(props) {
     super(props);
-  }
 
+    let antagonistsCount = antagonistCharacters.antagonists.length;
+    let randomElemIndex = Math.floor(Math.random() * antagonistsCount);
+
+    // setting the characters
+    let left = Math.floor(Math.random() * 2);
+    let charLeft = antagonistCharacters.antagonists[randomElemIndex][left];
+    let charRight = antagonistCharacters.antagonists[randomElemIndex][1 - left];
+
+    let tmp=charLeft;
+    charLeft=antagonistCharacters.characters[charLeft];
+    charLeft.img= "ForTheThrone/img/" +tmp + ".png";
+    tmp=charRight;
+    charRight=antagonistCharacters.characters[charRight];
+    charRight.img="ForTheThrone/img/" +tmp + ".png";
+
+    this.state = {
+      charLeft: charLeft,
+      charRight: charRight
+    };
+  }
+  
   render() {
     return (
       <div id="home">
-        <Row>
-          <Col>
-            <ForTheThrone />
-          </Col>
-        </Row>
+        <ForTheThrone charLeft={this.state.charLeft} charRight={this.state.charRight} />
+
         <Grid className="content">
           <Row >
             <Col md={6}>
@@ -39,12 +54,19 @@ export default class Start extends Component {
               </div>
             </Col>
           </Row>
+          <hr/>
+          <Row >
+            <Col>
+            todo
+            </Col>
+          </Row>
+          <hr/>
           <Row>
-            <br />
-            <Col md={8} mdPush={2}>
-              <div className="homeBlog">
-                <div dangerouslySetInnerHTML={{ __html: HomepageBlog }} />
-              </div>
+            <Col md={12} >
+              {/* <div className="card"> */}
+              <h3>{this.state.charLeft.name}'s and {this.state.charRight.name}'s location-history on a map:</h3>
+              <MapComp character={[this.state.charLeft.name, this.state.charRight.name]}/>
+              {/* </div> */}
             </Col>
           </Row>
         </Grid>
