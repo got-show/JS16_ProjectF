70d727c88547bd8b978089e3550dcc70f6d77a41
Taylor Lei <taylor.lei@tum.de>
2019-04-07 15:06:41 +0200

    removed chinese comment

--- app/components/common/ForTheThrone/img/index-generator.php
+++ app/components/common/ForTheThrone/img/index-generator.php
@@ -1,11 +1,10 @@
 <?php
-//获取某目录下所有文件、目录名（不包括子目录下文件、目录名）
-//获取某目录下所有文件、目录名（不包括子目录下文件、目录名）
+// this file generates the index.js file which imports all images from a given img folder
 
 $dir="./";
 $files=[];
 $handler = opendir($dir);
-while (($filename = readdir($handler)) !== false) {//务必使用!==，防止目录下出现类似文件名“0”等情况
+while (($filename = readdir($handler)) !== false) {
   if ($filename != "." && $filename != "..") {
       $files[] = $filename ;
       }
