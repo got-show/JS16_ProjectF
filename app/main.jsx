21eda79bd247ae53e3bb608f2f5e3435fdd19305
Boning Li <Boning.li@outlook.com>
2019-04-14 17:11:57 +0200

    bug fix

--- app/main.jsx
+++ app/main.jsx
@@ -38,7 +38,7 @@ browserHistory.listen(location => {
 
 function scrollBack() {
   history.scrollRestoration = 'manual';
-  if (!window.location.href.includes("/characters/?")||window.location.href.includes("page=1")) {
+  if (!window.location.href.includes("/characters?")||window.location.href.includes("page=1")) {
     window.scrollTo(0, 0);
   }
 }
